c73354cc6a0f1edbd198bc758c19b38150ce228f
SED4906 <sed4906birdie@gmail.com>
2024-05-19 16:17:38 -0700

    *sigh*

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use std::error::Error;
+use std::{error::Error, ffi::c_void, ptr::null};
+pub mod buffers;
 pub mod geometry;
 pub mod shader;
-pub mod buffers;
 pub mod texture;
 
 fn find_sdl_gl_driver() -> Option<u32> {
@@ -22,22 +22,58 @@ pub fn clear(red: f32, green: f32, blue: f32, alpha: f32) {
 
 pub struct Application();
 
+extern "system" fn error_handler(
+    source: u32,
+    type_: u32,
+    id: u32,
+    severity: u32,
+    length: i32,
+    message: *const i8,
+    _: *mut c_void,
+) {
+    print!("error! source {source} type {type_} id {id} severity {severity} ");
+    unsafe {
+        println!(
+            "{}",
+            String::from_utf8_lossy(core::slice::from_raw_parts(
+                message as *const u8,
+                length as usize
+            ))
+        );
+    }
+}
+
 impl Application {
-    pub fn run(title: &str, width: u32, height: u32, runner: impl Fn(sdl2::EventPump, sdl2::render::Canvas<sdl2::video::Window>) -> Result<(), Box<dyn Error>>) -> Result<(), Box<dyn Error>> {
+    pub fn run(
+        title: &str,
+        width: u32,
+        height: u32,
+        runner: impl Fn(
+            sdl2::EventPump,
+            sdl2::render::Canvas<sdl2::video::Window>,
+        ) -> Result<(), Box<dyn Error>>,
+    ) -> Result<(), Box<dyn Error>> {
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
-        let window = video_subsystem.window(title, width, height)
+        // video_subsystem.gl_attr().set_context_flags().debug().set();
+        let window = video_subsystem
+            .window(title, width, height)
             .opengl() // this line DOES NOT enable opengl, but allows you to create/get an OpenGL context from your window.
             .build()
             .unwrap();
-        let canvas = window.into_canvas()
+        let canvas = window
+            .into_canvas()
             .index(find_sdl_gl_driver().unwrap())
             .build()
             .unwrap();
-    
+
         // initialization
         gl::load_with(|name| video_subsystem.gl_get_proc_address(name) as *const _);
-    
+        // unsafe {
+        //     gl::Enable(gl::DEBUG_OUTPUT);
+        //     gl::DebugMessageCallback(Some(error_handler), null())
+        // }
+
         // sdl::render creates a context for you, if you use a Canvas you need to use it.
         canvas.window().gl_set_context_to_current().unwrap();
         let event_pump = sdl_context.event_pump().unwrap();
--- src/shader.rs
+++ src/shader.rs
@@ -17,13 +17,47 @@ impl Shader {
             let vertex = gl::CreateShader(gl::VERTEX_SHADER);
             let fragment = gl::CreateShader(gl::FRAGMENT_SHADER);
             let program = gl::CreateProgram();
-            gl::ShaderSource(vertex, 1, &vertex_source.as_bytes().into_iter().map(|b| *b as i8).collect::<Vec<i8>>().as_ptr(), null());
+            let mut vertex_source = vertex_source.as_bytes().into_iter().map(|b| *b as i8).collect::<Vec<i8>>();
+            vertex_source.push(0);
+            let mut fragment_source = fragment_source.as_bytes().into_iter().map(|b| *b as i8).collect::<Vec<i8>>();
+            fragment_source.push(0);
+            gl::ShaderSource(vertex, 1, &vertex_source.as_ptr(), null());
             gl::CompileShader(vertex);
-            gl::ShaderSource(fragment, 1, &fragment_source.as_bytes().into_iter().map(|b| *b as i8).collect::<Vec<i8>>().as_ptr(), null());
+            // let mut compiled = 0;
+            // let mut max_length = 0;
+            // gl::GetShaderiv(vertex, gl::COMPILE_STATUS, &mut compiled);
+            // gl::GetShaderiv(vertex, gl::INFO_LOG_LENGTH, &mut max_length);
+            // if compiled == 0 {
+            //     let mut bytes = vec![0u8;max_length as usize];
+            //     gl::GetShaderInfoLog(vertex, max_length, &mut max_length, bytes.as_mut_ptr() as *mut i8);
+            //     println!("{}", String::from_utf8(bytes).unwrap());
+            //     gl::DeleteShader(vertex);
+            // }
+            gl::ShaderSource(fragment, 1, &fragment_source.as_ptr(), null());
             gl::CompileShader(fragment);
+            // let mut compiled = 0;
+            // let mut max_length = 0;
+            // gl::GetShaderiv(fragment, gl::COMPILE_STATUS, &mut compiled);
+            // gl::GetShaderiv(fragment, gl::INFO_LOG_LENGTH, &mut max_length);
+            // if compiled == 0 {
+            //     let mut bytes = vec![0u8;max_length as usize];
+            //     gl::GetShaderInfoLog(fragment, max_length, &mut max_length, bytes.as_mut_ptr() as *mut i8);
+            //     println!("{}", String::from_utf8(bytes).unwrap());
+            //     gl::DeleteShader(fragment);
+            // }
             gl::AttachShader(program, vertex);
             gl::AttachShader(program, fragment);
             gl::LinkProgram(program);
+            // let mut linked = 0;
+            // let mut max_length = 0;
+            // gl::GetProgramiv(program, gl::LINK_STATUS, &mut linked);
+            // gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut max_length);
+            // if linked == 0 {
+            //     let mut bytes = vec![0u8;max_length as usize];
+            //     gl::GetProgramInfoLog(program, max_length, &mut max_length, bytes.as_mut_ptr() as *mut i8);
+            //     println!("{}", String::from_utf8(bytes).unwrap());
+            //     gl::DeleteProgram(program);
+            // }
             gl::DeleteShader(vertex);
             gl::DeleteShader(fragment);
             Self { program, vertex_attributes }
